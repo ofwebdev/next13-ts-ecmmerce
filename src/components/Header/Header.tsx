8862b03d9963e58b6831a5961d2861f829a42fa7
ofwebdev <nkomorfaruk@gmail.com>
2023-08-07 02:07:46 +0600

    Next 13 page route typescript project

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+
+// Imported img
+import logo from "../../images/logo.png";
+import cartIcon from "../../images/cartIcon.png";
+
+// Next default
+import Image from "next/image";
+import Link from "next/link";
+
+// icon
+import { BiCaretDown } from "react-icons/bi";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlLocationPin } from "react-icons/sl";
+
+const Header = () => {
+  return (
+    <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50">
+      <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4">
+        {/* Logo link  */}
+        <Link
+          href={"/"}
+          className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-center h-[70%]"
+        >
+          <Image className="w-28 object-cover mt-1" src={logo} alt="logoImg" />
+        </Link>
+
+        {/* delivery */}
+        <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 items-center justify-center h-[70%] hidden xl:inline-flex gap-1">
+          <SlLocationPin />
+          <div className="text-xs">
+            <p>Deliver to</p>
+            <p className="text-white font-bold uppercase">USA</p>
+          </div>
+        </div>
+
+        {/* serchbar */}
+        <div className="flex-1 h-10 hidden md:inline-flex items-center justify-between relative">
+          <input
+            // onChange={handleSearch}
+            // value={searchQuery}
+            className="w-full h-full rounded-md px-2 placeholder:text-sm text-base text-black border-[3px] border-transparent outline-none focus-visible:border-amazon_yellow"
+            type="text"
+            placeholder="Search next amazon"
+          />
+          <span className="w-12 h-full bg-amazon_yellow text-black text-2xl flex items-center justify-center absolute right-0 rounded-tr-md rounded-br-md">
+            <HiOutlineSearch />
+          </span>
+        </div>
+
+        <div
+          // onClick={() => signIn()}
+          className="text-xs text-gray-100 flex flex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%]"
+        >
+          <p>Hello, sign in</p>
+          <p className="text-white font-bold flex items-center">
+            Account & Lists
+            <span>
+              <BiCaretDown />
+            </span>
+          </p>
+        </div>
+
+        {/* favorite */}
+        <Link
+          href={"/favorite"}
+          className="text-xs text-gray-100 flex flex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%] relative"
+        >
+          <p>Marked</p>
+          <p className="text-white font-bold">& Favorite</p>
+          {/* {favoriteData.length > 0 && (
+            <span className="absolute right-2 top-2 w-4 h-4 border-[1px] border-gray-400 flex items-center justify-center text-xs text-amazon_yellow">
+              {favoriteData.length}
+            </span>
+          )} */}
+        </Link>
+
+        {/* cart */}
+        <Link
+          href={"/cart"}
+          className="flex items-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%] relative"
+        >
+          <Image
+            className="w-auto object-cover h-8"
+            src={cartIcon}
+            alt="cartImg"
+          />
+          <p className="text-xs text-white font-bold mt-3">Cart</p>
+          <span className="absolute text-amazon_yellow text-sm top-2 left-[29px] font-semibold">
+            {/* {productData ? productData.length : 0} */}
+          </span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import RootLayout from "@/components/layout/RootLayout";
+import "@/styles/globals.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RootLayout>
+      <Component {...pageProps} />
+    </RootLayout>
+  );
 }
