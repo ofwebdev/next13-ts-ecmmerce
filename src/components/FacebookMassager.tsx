13bcde04a2a4699bd2ea7aede0104aa2dfa11c27
ofwebdev <nkomorfaruk@gmail.com>
2023-08-20 23:15:34 +0600

    Build error fix try to do

--- src/components/FacebookMassager.tsx
+++ src/components/FacebookMassager.tsx
@@ -1,6 +1,7 @@
 // "use client";
 
 import { FacebookProvider, CustomChat } from "react-facebook";
+import { SiTruenas } from "react-icons/si";
 
 export default function FacebookMessenger() {
   const appId = process.env.appId as string;
@@ -9,7 +10,7 @@ export default function FacebookMessenger() {
   console.log(appId);
   return (
     <FacebookProvider appId={appId} chatSupport>
-      <CustomChat pageId={pageId} minimized={false} />
+      <CustomChat pageId={pageId} minimized={SiTruenas} />
     </FacebookProvider>
   );
 }
